82e460432f337acb8aee9e16614e1c8a192ad7b3
Alex <alejandro.rosadoperez94@gmail.com>
2018-10-01 12:55:41 +0200

    - Creación del proyecto usando Spring Initializr

---
+++ ProjectAtS/src/main/java/com/ats/ProjectAtS/ProjectAtSApplication.java
@@ -0,0 +1,12 @@
+package com.ats.ProjectAtS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAtSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAtSApplication.class, args);
+	}
+}
